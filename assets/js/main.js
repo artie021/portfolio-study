1f8acad9b4524862034d871943741b2ec13d4c66
Ａｒｔｉｅ_ <arthurfellype21@gmail.com>
2021-04-05 03:50:58 -0300

    Update files

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ const showMenu = (toggleId, navId) =>{
             nav.classList.toggle('show')
         })
 
-    }
+    } 
 }
 showMenu('nav-toggle','nav-menu')
 
@@ -69,6 +69,4 @@ sr.reveal('.contact__subtitle', {})
 sr.reveal('.contact__text', {interval:200})
 sr.reveal('.contact__input', {delay: 400})
 sr.reveal('.contact__button', {delay:600})
-
-
 
